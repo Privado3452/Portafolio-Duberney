5d4cab51a00c04e17cc64f9409dd188768c0b569
Duberney Lopez <duberneylopezlopez1@gmail.com>
2023-12-04 22:18:48 -0500

    Actualizacion

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(". hamburger-icon");
+  const icon = document.querySelector(".hamburger-icon");
   menu.classList.toggle("open");
   icon.classList.toggle("open");
+}
+
+
+function scrollToSection(contact) {
+  var section = document.getElementById(contact);
+  if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+  }
 }
